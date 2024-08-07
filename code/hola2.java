f65b4471e150e9ae7db583c256f55973d943d06a
Diego Pazaran Rodriguez <dpazaran@gmail.com>
2024-08-06 22:31:36 -0600

    build 2

--- code/hola2.java
+++ code/hola2.java
@@ -3,7 +3,6 @@ import org.apache.commons.lang3.StringUtils;
 public class hola2 {
     public static void main(String[] args) {
         String mensaje = "Hola, Mundo";
-        String mensajeInvertido = StringUtils.reverse(mensaje);
         System.out.println("otro holamundo");
     }
 }
