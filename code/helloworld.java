03c8cc1116522c4a44b2155b8b65dca2aa9f1c8f
Diego Pazaran Rodriguez <dpazaran@gmail.com>
2024-08-06 22:38:24 -0600

    cambios 3

--- code/helloworld.java
+++ code/helloworld.java
@@ -2,6 +2,6 @@ public class helloworld {
     public static void main(String[] args) {
         String mensaje = "Hola, Mundo";
         String mensajeInvertido = StringUtils.reverse(mensaje);
-        System.out.println("otro holamundo");
+        System.out.println("otro hola mundo");
     }
 }
